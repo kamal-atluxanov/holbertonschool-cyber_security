8c9503f73de1684d21273f5bc360b4899d99ffd9
Kamal <atluxanovvkamal@gmail.com>
2026-05-19 03:54:16 -0400

    Ruby

--- scripting_cyber/0x00-ruby_scripting/3-main.rb
+++ scripting_cyber/0x00-ruby_scripting/3-main.rb
@@ -1,5 +1,26 @@
-#!/usr/bin/env ruby
+require 'json'
 
-require_relative '3-read_file'
+def count_user_ids(path)
+  # 1. JSON faylını oxuyuruq və analiz (parse) edirik
+  file_content = File.read(path)
+  data = JSON.parse(file_content)
 
-count_user_ids('file.json')
+  # 2. Hər bir userId-nin neçə dəfə təkrarlandığını saymaq üçün Hash yaradırıq
+  # Default olaraq tapılmayan açarların dəyərini 0 təyin edirik
+  counts = Hash.new(0)
+
+  data.each do |item|
+    if item['userId']
+      counts[item['userId']] += 1
+    end
+  end
+
+  # 3. Nəticəni ekrana çıxarırıq (Məsələn, "1: 10")
+  counts.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
+rescue Errno::ENOENT
+  puts "Xəta: #{path} faylı tapılmadı!"
+rescue JSON::ParserError
+  puts "Xəta: Fayl düzgün JSON formatında deyil!"
+end
