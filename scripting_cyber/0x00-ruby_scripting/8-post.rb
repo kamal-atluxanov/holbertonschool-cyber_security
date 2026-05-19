ad287cd160bedbf72c7ea6e3ddc43f283a0d525f
Kamal <atluxanovvkamal@gmail.com>
2026-05-19 04:44:21 -0400

    Ruby

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,23 +1,25 @@
-#!/usr/bin/env ruby
 require 'net/http'
 require 'uri'
 require 'json'
 
 def post_request(url, body_params = {})
+  # 1. URL-i analiz edirik
   uri = URI.parse(url)
 
-  # Create an HTTP POST request
+  # 2. HTTP POST sorğusunu hazırlayırıq
   request = Net::HTTP::Post.new(uri)
-  request.content_type = 'application/json'
-  request.body = body_params.to_json unless body_params.empty?
+  request['Content-Type'] = 'application/json'
+  request.body = body_params.to_json
 
-  # Initialize the HTTP session
+  # 3. Sorğunu göndəririk
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
     http.request(request)
   end
 
-  # Print response status and body
+  # 4. Tapşırıqdakı formata uyğun olaraq nəticəni çap edirik
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts JSON.pretty_generate(JSON.parse(response.body))
+  puts response.body
+rescue StandardError => e
+  puts "Xəta baş verdi: #{e.message}"
 end
